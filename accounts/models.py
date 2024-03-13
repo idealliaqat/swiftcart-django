6dcd09979b112d78d8624797e3e3fbf4f4303ff4
idealliaqat <idealliaqatpucit786@gmail.com>
2024-03-13 18:56:40 +0500

    accounts integrate

--- accounts/models.py
+++ accounts/models.py
@@ -42,17 +42,18 @@ class AccountManager(BaseUserManager):
 class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, validators=[EmailValidator(message='Enter a valid email address.')])
     username = models.CharField(max_length=30, unique=True)
-    phone_number = PhoneNumberField(
-        validators=[phone_number_validator],
-        help_text=_('Enter your 11-digit phone number with digits only.')
-    )
+    # phone_number = PhoneNumberField(
+    # validators=[phone_number_validator],
+    # help_text=_('Enter your 11-digit phone number with digits only.')
+    # )
+    phone_number = models.CharField(validators=[phone_number_validator], max_length=12, blank=False)
     first_name = models.CharField(max_length=30, blank=False)
     last_name = models.CharField(max_length=30, blank=False)
     full_name = models.CharField(max_length=60, blank=True)
     # required (set these fields on user creation)
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(default=timezone.now)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
